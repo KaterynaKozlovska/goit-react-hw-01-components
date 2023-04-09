d2099ee6f76c66494d880924ebe2fdfd39dc9297
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-04-09 19:27:32 +0300

    corect profile and statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
+import Profile from './profile';
+import Statistics from './statistics';
+import data from 'data.json';
+import user from 'user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
+  );
+}
---
+++ src/components/statistics.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => (
+  <section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
+
+    <ul className="statList">
+      {stats.map(stat => (
+        <li className="item" key={stat.id}>
+          <span className="label">{stat.label}</span>
+          <span className="percentage">{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default Statistics;
